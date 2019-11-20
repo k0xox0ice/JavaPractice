58afd60e3d53267e2ff4d78575bf1dfbe9d1e56f
Kazuya Kurihara <k-kurihara@cmknet.co.jp>
2019-11-20 16:03:28 +0900

    【11/20】 ファイル作成の参考URL

---
+++ src/practice/MakeFile.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class MakeFile {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//ファイルを作成
+		//createNewFileメソッドを使用する
+		//参考URL: https://www.sejuku.net/blog/20527
+	}
+
+}
