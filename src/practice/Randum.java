6f8581e19b0b8ffa60ee0629444d88a65bfbf593
Kazuya Kurihara <k-kurihara@cmknet.co.jp>
2019-12-26 18:14:23 +0900

    【12/26】 ランダム関数 重複ナシ 完成

--- src/practice/Randum.java
+++ src/practice/Randum.java
@@ -9,19 +9,14 @@ public class Randum {
 
         int count = 0;
         int r = 0;
-        int r2 = r;
         int listNumbers[] = {4, 4, 4};
 
 		while (count < 3) {
 
 			r = Random();
 
-			while (r == listNumbers[0] && r == listNumbers[1] && r == listNumbers[2]) {
-				r2 = Random();
-			}
-
-			if (r != r2) {
-				r2 = r;
+			while (r == listNumbers[0] || r == listNumbers[1] || r == listNumbers[2]) {
+				r = Random();
 			}
 
 			listNumbers[count] = r;
